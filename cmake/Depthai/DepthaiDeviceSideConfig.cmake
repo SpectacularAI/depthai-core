c5c3c2c07c1fd805cc0bef1845c7388fe089730f
TheMarpe <martin.peterlin7@gmail.com>
2021-07-27 03:36:22 +0200

    Updated FW with GPIO interrupts support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e88cdb4afa8be3cbbf8c739601f1aa1b2fd5395b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "085aada9b6860e9ccc4b31ac4453d86ec21ec921")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
