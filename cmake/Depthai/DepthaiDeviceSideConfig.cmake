081940b4c2f1ca97d363e718ffa36fe63133d6ea
Martin Peterlin <martin.peterlin7@gmail.com>
2021-07-30 02:28:33 +0200

    Updated FW with SPI Fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "085aada9b6860e9ccc4b31ac4453d86ec21ec921")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d44da0c71ecef1aecb9dcd902bc7416495b4b5d3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
