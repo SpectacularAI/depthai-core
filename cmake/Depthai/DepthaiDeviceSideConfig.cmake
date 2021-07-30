51b9079ad1c6c97c9b2c0563cea0d228dfb1a7b9
Martin Peterlin <martin.peterlin7@gmail.com>
2021-07-30 16:46:34 +0200

    Updated FW with SPIOut speed improvements

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d44da0c71ecef1aecb9dcd902bc7416495b4b5d3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "eb5770f748fe7918024955da180937a92cf5da5c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
