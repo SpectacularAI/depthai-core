19a5eda3d3cf4f194232f1ca4e0cf9469720375c
alex-luxonis <alex@luxonis.com>
2021-08-12 14:23:05 +0300

    Update FW: enable conditioned disparity output for subpixel. Note: it may affect the fractional disparity interpolation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a2baa42ba41f7b79dcbee987b34f24da075de196")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "47682492138d3bcfa4e1097d20e8a659736511e4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
