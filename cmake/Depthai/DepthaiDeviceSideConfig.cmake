40bba3c37dd56e75a5a93dc4dff31790b424e35c
alex-luxonis <alex@luxonis.com>
2021-08-27 03:06:49 +0300

    Update FW: disparity/depth align: disable warp rescale interpolation, set to direct pixel replication. As we have invalid pixels set to 0, interpolation with bicubic or bilinear would create false values

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1928f3407397272cc9736aedcca667c6c7419057")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6c499bc415d00f4f42951c9c8e1369ae6f32d273")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
